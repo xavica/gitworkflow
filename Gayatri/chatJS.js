9479ff9d68be8e11022b3969412b9bfe82285092
unknown <gayatri.sarva@xavica.com>
2015-02-16 15:52:13 +0530

    Chat G3

--- Gayatri/chatJS.js
+++ Gayatri/chatJS.js
@@ -2,11 +2,10 @@
 function chatFunction() 
 
 {
-	var msg = document.getElementById("input_box");
-	var cmsg = document.getElementById("chat_box");
-
-	document.getElementById("chat_box").value = cmsg.value + msg.value;
-	document.getElementById("input_box").value = "";
+	var msg = document.getElementById('input_box');       
+      
+    document.getElementById('section').innerHTML = document.getElementById('section').innerHTML  +'</br> </br>'+  msg.value;
+    document.getElementById('input_box').value="";
 
 
 
