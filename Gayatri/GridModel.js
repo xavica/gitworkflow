e2ef3bda898a7f82a3ce2cd56ffa0a6c628511d1
unknown <gayatri.sarva@xavica.com>
2015-03-22 11:07:09 +0530

    Status Stringg#

--- Gayatri/GridModel.js
+++ Gayatri/GridModel.js
@@ -1,5 +1,6 @@
-function Employee(contactName,contactTitle,companyName,country)
+function Employee(empId,contactName,contactTitle,companyName,country)
 {
+	this.empId = empId;
 	this.contactName = contactName;
 	this.contactTitle = contactTitle;
 	this.companyName = companyName;
@@ -10,34 +11,59 @@ function EmployeeGridViewModel()
 {
 	var that = this;
 	this.sourceEmployees =[
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee("Maria Anders","sales Representative", "Alfreds Futterkiste","Germany"))
+						( new Employee(1,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(2,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(3,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(4,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(5,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(6,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(7,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(8,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(9,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(10,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(1,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(2,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(3,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(4,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(5,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(6,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(7,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(8,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(9,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee(10,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany"))
 
 
 					];
 	that.pageSelection = {
-					       pages: ['1','2','5','7'],
-					        pageSize: ko.observable('5')
+					       pages: [5,10,15,20],
+					        pageSize: ko.observable(0)
     				};
     that.employees = ko.observableArray(_.slice(that.sourceEmployees,0,that.pageSelection.pageSize()));
-
+    that.pageNumber = ko.observable(1);
     that.pageSelection.pageSize.subscribe(function()
-    {
-    	console.log(that.pageSelection.pageSize());
-    	that.employees(_.slice(that.sourceEmployees,0,that.pageSelection.pageSize()));
+    {      	
+    	var start = +((that.pageNumber() - 1 ) * +that.pageSelection.pageSize()) + 1;
+    	var end = +start + +that.pageSelection.pageSize() - 1;
+    	that.statusString(start + " - " + end + ' of ' +  that.sourceEmployees.length);
+    	that.employees(_.slice(that.sourceEmployees,start-1,end));
 
+    	var pageNumbersCount = ( that.sourceEmployees.length / +that.pageSelection.pageSize() ) 
+    	console.log(pageNumbersCount);
+    	that.pageNumbers = ko.observableArray(_.times(pageNumbersCount,function(n)
+    		{
+    			return n+1;
+    		}));
+    	console.log(that.pageNumbers());
+
+( that.sourceEmployees.length % that.pageSelection.pageSize()) ) >0 ?  1 : 0;
+
+    	
     });
+    that.statusString = ko.observable('');
+    that.pageSelection.pageSize(5);
 
 
-
+    
 
 }
 var emp = new EmployeeGridViewModel();
