39d718e64a6466830fb952d218bcf69a29097939
unknown <gayatri.sarva@xavica.com>
2015-03-23 21:31:19 +0530

    CRUD in Telerik - G3

--- Gayatri/GridModel.js
+++ Gayatri/GridModel.js
@@ -6,39 +6,35 @@ function Employee(empId,contactName,contactTitle,companyName,country)
 	this.companyName = companyName;
 	this.country = country;
 }
-function ButtonColor(buttonNumber,buttonStatus)
-{
-	this.buttonNumber = buttonNumber;
-	this.buttonStatus = buttonStatus;
-}
+
 
 function EmployeeGridViewModel()
 {
 	var that = this;
 	this.sourceEmployees =[
-						( new Employee(1,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(2,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(3,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(4,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(5,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(6,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(7,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(8,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(9,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(10,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(11,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(12,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(13,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(14,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(15,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(16,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(17,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(18,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(19,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(20,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(21,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(22,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
-						( new Employee(23,"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany"))
+						( new Employee('1',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('2',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('3',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('4',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('5',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('6',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('7',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('8',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('9',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('10',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('11',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('12',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('13',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('14',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('15',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('16',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('17',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('18',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('19',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('20',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('21',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('22',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany")),
+						( new Employee('23',"Maria Anders","sales Representative", "Alfreds Futterkiste","Germany"))
 
 
 					];
@@ -75,6 +71,56 @@ function EmployeeGridViewModel()
 	 }
     that.statusString = ko.observable('');
     that.pageSelection.pageSize(3);
+    that.displayRecord = ko.observable(new Employee('','','','',''));
+    that.isEdit = ko.observable(false);
+    that.isNew = ko.observable(false);
+
+    that.clickDisplay = function(clickedRow)
+	{
+		that.displayRecord(clickedRow);
+		that.isEdit(true);
+		
+	}
+	this.clickNew = function()
+	{
+		that.displayRecord(getEmptyObject());
+		that.isEdit(true);
+	}
+
+	that.clickSave = function()
+	{
+		that.sourceEmployees.push(that.displayRecord());
+		that.isEdit(false);
+		that.displayRecord(getEmptyObject());
+		that.isNew(false);
+		
+	}
+	that.clickUpdate = function(clickedRow)
+	{
+		var k = that.sourceEmployees.indexOf(clickedRow);
+		that.sourceEmployees.splice(k,1,that.displayRecord());
+		that.buttonClicked(1);
+		that.displayRecord(getEmptyObject());
+		that.isNew(false);
+		that.isEdit(false);
+	}
+
+	that.clickDelete = function(clickedRow)
+	{
+		that.displayRecord(clickedRow);
+		var k = that.sourceEmployees.indexOf(clickedRow);
+		that.sourceEmployees.splice(k,1);
+		that.buttonClicked(1);
+		that.isEdit(false);
+		that.displayRecord(getEmptyObject());
+		that.isNew(false);
+	}
+	function getEmptyObject()
+	{
+		return new Employee('','','','','');
+	}
+
+
 
 }
 var emp = new EmployeeGridViewModel();
