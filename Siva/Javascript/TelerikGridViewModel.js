78c556486ee75b5d9667c8f51e4588d54b057687
unknown <siva.jonnala@xavica.com>
2015-03-25 08:48:24 +0530

    grid model

--- Siva/Javascript/TelerikGridViewModel.js
+++ Siva/Javascript/TelerikGridViewModel.js
@@ -3,12 +3,14 @@ function Header(headerName)
 	this.headerName=headerName;
 };
 
-function Mobile(mobileCompany,mobileModel,mobileCost,mobileOS)
-{
+function Mobile(slNo,mobileCompany,mobileModel,mobileCost,mobileOS)
+{	this.slNo = slNo;
+	// this.slNo.length= length+1;
 	this.mobileCompany = mobileCompany;
 	this.mobileModel = mobileModel;
 	this.mobileCost = mobileCost;
 	this.mobileOS = mobileOS;
+	
 };
 function MenuBar(topMenuBar,sideMenuBar)
 {
@@ -23,6 +25,10 @@ function SideMenu(sideMenu)
 { 
 	this.sideMenu = sideMenu 
 };
+function DropDownList(listItems)
+{
+	this.listItems = listItems;
+}
 function MobileGridViewModel()
 {
 	this.topMenuBars = [	new TopMenu('| Home |'),new TopMenu(' Best Deals |'),new TopMenu('MostViewProducts |'),
@@ -31,19 +37,74 @@ function MobileGridViewModel()
 	this.sideMenuBars = [	new SideMenu('Mobile Products'),new SideMenu('Mens Accesories'),
 							new SideMenu('Women Accesories'),new SideMenu('Other')
 						];
-
-	this.headers =	[	new Header('Company'),
+	var	that=this, localMobileData, headers;
+	that.headers =	[	new Header('Sl.No.'),
+						new Header('Company'),
 						new Header('Model'),
 						new	Header('Cost'),
 						new Header('Operating System')
 					];
-	var	that=this, localMobileData;
-	localMobileData=	[	new Mobile('Apple','IPhone 6S','45000/-','IOS'),new Mobile('Samsung','Galxy Note III','25000/-','Android'),
-						new Mobile('Nokia','Lumia 730','20000/-','Windows 8'),new Mobile('Sony','Xperia M','13000/-','Android'),
-						new Mobile('Motorola','Moto X','18000/-','Android'),new Mobile('MicroMax','Yurekha','10000/-','Android'),
-						new Mobile('Celcon','Celcon Smart','7000/-','Android'),new Mobile('HTC','HTC Smart Phone','10000/-','Andoid'),
-					]
-	that.mobiles=ko.observableArray(localMobileData);
+	
+	localMobileData =	[		new Mobile((length++)+1,'Apple','IPhone 6S','45000/-','IOS'),	new Mobile((length++)+1,'Samsung','Galxy Note III','25000/-','Android'),
+								new Mobile((length++)+1,'Nokia','Lumia 730','20000/-','Windows 8'),	new Mobile((length++)+1,'Sony','Xperia M','13000/-','Android'),
+								new Mobile((length++)+1,'Motorola','Moto X','18000/-','Android'),	new Mobile((length++)+1,'MicroMax','Yurekha','10000/-','Android'),
+								new Mobile((length++)+1,'Celcon','Celcon Smart','7000/-','Android'),	new Mobile((length++)+1,'HTC','HTC Smart Phone','10000/-','Andoid'),
+								new Mobile((length++)+1,'Apple','IPhone 6S','45000/-','IOS'),	new Mobile((length++)+1,'Samsung','Galxy Note III','25000/-','Android'),
+								new Mobile((length++)+1,'Nokia','Lumia 730','20000/-','Windows 8'),	new Mobile((length++)+1,'Sony','Xperia M','13000/-','Android'),
+								new Mobile((length++)+1,'Motorola','Moto X','18000/-','Android'),	new Mobile((length++)+1,'MicroMax','Yurekha','10000/-','Android'),
+								new Mobile((length++)+1,'Celcon','Celcon Smart','7000/-','Android'),	new Mobile((length++)+1,'HTC','HTC Smart Phone','10000/-','Andoid'),
+								new Mobile((length++)+1,'Apple','IPhone 6S','45000/-','IOS'),	new Mobile((length++)+1,'Samsung','Galxy Note III','25000/-','Android'),
+								new Mobile((length++)+1,'Nokia','Lumia 730','20000/-','Windows 8'),	new Mobile((length++)+1,'Sony','Xperia M','13000/-','Android'),
+								new Mobile((length++)+1,'Motorola','Moto X','18000/-','Android'),	new Mobile((length++)+1,'MicroMax','Yurekha','10000/-','Android'),
+								new Mobile((length++)+1,'Celcon','Celcon Smart','7000/-','Android'),	new Mobile((length++)+1,'HTC','HTC Smart Phone','10000/-','Andoid'),
+								new Mobile((length++)+1,'Apple','IPhone 6S','45000/-','IOS'),	new Mobile((length++)+1,'Samsung','Galxy Note III','25000/-','Android'),
+								new Mobile((length++)+1,'Nokia','Lumia 730','20000/-','Windows 8'),	new Mobile((length++)+1,'Sony','Xperia M','13000/-','Android'),
+								new Mobile((length++)+1,'Motorola','Moto X','18000/-','Android'),	new Mobile((length++)+1,'MicroMax','Yurekha','10000/-','Android'),
+								new Mobile((length++)+1,'Celcon','Celcon Smart','7000/-','Android'),	new Mobile((length++)+1,'HTC','HTC Smart Phone','10000/-','Andoid'),
+								new Mobile((length++)+1,'Apple','IPhone 6S','45000/-','IOS'),	new Mobile((length++)+1,'Samsung','Galxy Note III','25000/-','Android'),
+								new Mobile((length++)+1,'Nokia','Lumia 730','20000/-','Windows 8'),	new Mobile((length++)+1,'Sony','Xperia M','13000/-','Android'),
+								new Mobile((length++)+1,'Motorola','Moto X','18000/-','Android'),	new Mobile((length++)+1,'MicroMax','Yurekha','10000/-','Android'),
+								new Mobile((length++)+1,'Celcon','Celcon Smart','7000/-','Android'),	new Mobile((length++)+1,'HTC','HTC Smart Phone','10000/-','Andoid'),
+								new Mobile((length++)+1,'Apple','IPhone 6S','45000/-','IOS'),	new Mobile((length++)+1,'Samsung','Galxy Note III','25000/-','Android'),
+								new Mobile((length++)+1,'Nokia','Lumia 730','20000/-','Windows 8'),	new Mobile((length++)+1,'Sony','Xperia M','13000/-','Android'),
+								new Mobile((length++)+1,'Motorola','Moto X','18000/-','Android'),	new Mobile((length++)+1,'MicroMax','Yurekha','10000/-','Android'),
+								new Mobile((length++)+1,'Celcon','Celcon Smart','7000/-','Android'),	new Mobile((length++)+1,'HTC','HTC Smart Phone','10000/-','Andoid')
+							
+						];
 
+	that.pageSelection = {
+					       	pages: [5,10,20,50],
+					        pageSize: ko.observable(0)
+    					 }
+    that.mobiles = ko.observableArray(_.slice(localMobileData,0,that.pageSelection.pageSize()));
+	that.pageNumber = ko.observable(1);
+	that.pageNumbers = ko.observableArray([]);
+	that.selectedPage = ko.observable(that.buttonClicked);
+
+
+    that.pageSelection.pageSize.subscribe(function()
+    {
+    	
+    	that.buttonClicked(1);
+    	
+    });
+    that.buttonClicked = function(currentpage)
+    {	
+    	var start = ((currentpage - 1 ) * that.pageSelection.pageSize()) + 1;
+    	var end = start + that.pageSelection.pageSize() - 1;
+    	if(end > localMobileData.length)
+    		end = localMobileData.length;
+    	that.statusString(start + " - " + end + ' of ' + localMobileData.length);
+    	that.mobiles(_.slice(localMobileData,start-1,end));
+    	var pageNumbersCount = Math.ceil(localMobileData.length / +that.pageSelection.pageSize() );
+    	
+    	that.pageNumbers(_.times(pageNumbersCount,function(n){ return n+1; }));
+    	that.selectedPage(false);
+
+    }
+    that.statusString = ko.observable('');
+    that.pageSelection.pageSize(5);
+    
 }
-ko.applyBindings(new MobileGridViewModel());
+var mo = new MobileGridViewModel();
+ko.applyBindings(mo);
