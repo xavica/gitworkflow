baf2c970dd97fbfbffd4f215c451f6c5365950e6
sarvagayatri <sarvagayatri@gmail.com>
2015-06-24 09:07:45 +0530

    vendor urls

--- Gayatri/CrawlerApi/FabFurnish.js
+++ Gayatri/CrawlerApi/FabFurnish.js
@@ -15,186 +15,186 @@ var fabFurnishLinks = [
     },
     isScroll: false,
     id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/wardrobes/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/outdoor/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/bar/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/l-shape-sofas/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/dining-sets/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/wall-shelves/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/dining-sideboards/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/ottomans-benches-stools/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/mirrors/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/dressing-tables/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/bookshelves/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
-},
-{
-    url: "http://www.fabfurnish.com/furniture/entertainment-tv-units/?sort=discount&dir=desc&discount_percent=1-90",
-    selectors: {
-        elements: '.productsCatalog .itm',
-        title: 'div a.itm-link',
-        description: '',
-        imageUrl: 'div a.itm-link img',
-        actualPrice: '.catItmPriceSection .oldprice',
-        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
-        discount: '.catItmPriceSection .itm-saleFlagPerNew',
-        redirectUrl: 'div a.itm-link'
-    },
-    isScroll: false,
-    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/wardrobes/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/outdoor/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/bar/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/l-shape-sofas/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/dining-sets/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/wall-shelves/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/dining-sideboards/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/ottomans-benches-stools/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/mirrors/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/dressing-tables/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/bookshelves/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
+//},
+//{
+//    url: "http://www.fabfurnish.com/furniture/entertainment-tv-units/?sort=discount&dir=desc&discount_percent=1-90",
+//    selectors: {
+//        elements: '.productsCatalog .itm',
+//        title: 'div a.itm-link',
+//        description: '',
+//        imageUrl: 'div a.itm-link img',
+//        actualPrice: '.catItmPriceSection .oldprice',
+//        sellingPrice: '.catItmPriceSection .itm-priceSpecial',
+//        discount: '.catItmPriceSection .itm-saleFlagPerNew',
+//        redirectUrl: 'div a.itm-link'
+//    },
+//    isScroll: false,
+//    id: 15
 }];
 
 var casper = require('casper').create();
---
+++ Gayatri/VendorCrawlers/crawler.js
@@ -0,0 +1,66 @@
+﻿/*jslint nomen: true, regexp: true, sloppy: true */
+/*global define, jQuery, $, document, require, exports, __utils__ */
+/* latedef: nofunc*/
+/*jshint onevar: true */
+
+var crawler = require('./xa-crawlerUtil.js'),
+    casper = require('casper').create(),
+    productsList = [],
+    vendors = [
+        //require('./Amazon.js').getVendor(),
+        //require('./BabyOye.js').getVendor(),   
+        //require('./BagItToday.js').getVendor(),
+        //require('./BookAdda.js').getVendor(),  // not retreiving products
+        //require('./CashKaro.js').getVendor(), 
+        //require('./CraftsVilla.js').getVendor(),
+        //require('./CromaRetail.js').getVendor(), 
+        //require('./DusKya.js').getVendor(),
+        //require('./Evok.js').getVendor(), // Explicit Discount
+         //require('./EzoneOnline.js').getVendor(), 
+         //require('./FabFurnish.js').getVendor(), //extracted 1042, not pushed
+         //require('./FashionAndyou.js').getVendor(),
+         //require('./FashionEra.js').getVendor(),
+         //require('./FirstCry.js').getVendor(), 
+          //require('./Flipkart.js').getVendor(), 
+            //require('./GiftEase.js').getVendor(), // not pushing products
+            //require('./GreenDust.js').getVendor(), // no explicit title property
+            //require('./HomeShop18.js').getVendor() // Explicit Discount
+            //require('./IndiaTimes.js').getVendor(),
+             //require('./InfiBeam.js').getVendor() //last 2 links not working.
+             //require('./InLiving.js').getVendor(),
+               //require('./Mirraw.js').getVendor(),
+               //require('./PepperFry.js').getVendor(), //no title property
+               //require('./RajMusical.js').getVendor(), // Explicit Discount
+               //require('./Saholic.js').getVendor(), // Explicit Discount
+               //require('./SbMusic.js').getVendor(), // Explicit Discount
+               //require('./ShopClues.js').getVendor(),  // few links not working
+               //require('./TvDeals.js').getVendor(), // Explicit Discount
+               //require('./Zansaar.js').getVendor() // extracted but not pushed into productstage.
+               // ebay, snapdeal, jabong, bajaao, gazetguru, 
+
+    ];
+
+casper.start();
+
+vendors.forEach(function (vendor) {
+    //parse the urls.
+    if (vendor) {
+        casper.then(function () {
+            productsList = crawler.parseUrls(vendor.links, casper);
+        });
+        casper.then(function () {
+            if (productsList) {
+                casper.echo('Vendor Name' + vendor.storeName + ", Total Products List:" + productsList.length);
+                // pushing items to ProductStage Table.
+                crawler.defaultPushToStage(productsList, vendor.storeName, casper);
+            }
+        });
+    }
+});
+
+casper.then(function () {
+    casper.echo('Finished all the vendors.');
+});
+
+
+casper.run();
