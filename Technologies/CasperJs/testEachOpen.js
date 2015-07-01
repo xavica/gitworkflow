360875560e8eb97fc176d718f88457077b0df9aa
xavica <xavicaservices@gmail.com>
2015-06-30 20:41:44 -0400

    Added capser eachopen for asynchtest

---
+++ Technologies/CasperJs/testEachOpen.js
@@ -0,0 +1,28 @@
+﻿
+
+var casper = require('casper').create();
+casper.start();
+
+
+// pushing Flipkart items to ProductStage Table.
+casper.then(function () {
+    var items = ['www.google.com', 'www.amazon.com', 'www.murthy.com', 'www.deals2buy.com'];
+    items.forEach(function (url) {
+        casper.thenOpen(url, {}, function () {
+            console.log(url + ' opened');
+            setTimeout(function () { 
+                console.log(i);
+            }, Math.floor((Math.random() * 500) + 1000));
+        });
+        casper.then(function () {
+            console.log(url + 'opened and after');
+        });
+    });
+    casper.then(function () {
+        casper.echo('outside all links');
+        casper.exit();
+    });
+});
+////Getting products from ProductStageTable
+
+casper.run();
