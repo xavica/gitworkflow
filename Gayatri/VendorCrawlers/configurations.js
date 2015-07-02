51e059d22df6e23a63a4ce59901daaa2f04307ab
xavica <xavicaservices@gmail.com>
2015-07-02 02:33:43 -0400

    added configurations js file

---
+++ Gayatri/VendorCrawlers/configurations.js
@@ -0,0 +1,14 @@
+﻿
+
+
+exports.configurations = {
+    "categories" : {
+        "laptops" : 1,
+        "cameras" : 2,
+        "toys" : 3
+    },
+    "urls": {
+        "productStageUrl" : "",
+        "assetsUrl" : ""
+    }
+};
