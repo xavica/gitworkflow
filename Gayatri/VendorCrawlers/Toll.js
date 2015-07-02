dab28eeb5a5f153bab2bdff483c65cd285353010
xavica <xavicaservices@gmail.com>
2015-07-02 10:57:07 -0400

    Fixed Jslint issues

--- Gayatri/VendorCrawlers/Toll.js
+++ Gayatri/VendorCrawlers/Toll.js
@@ -1,20 +1,28 @@
-﻿var request = require("request");
+﻿/*jslint nomen: true, regexp: true, sloppy: true */
+/*global define, console, require, exports, __utils__ */
+/*global append, exportToLog, updateProduct */
+/*jshint onevar: true */
+/*jslint white: true */
+/*jshint unused: true, node: true */
+/*jslint unparam: true, node: true */
+
+var request = require("request");
 var _ = require('lodash-node');
 var querystring = require('querystring');
 var http = require("http");
-fs = require('fs');
+var fs = require('fs');
 var configurations = require('./configurations.js').configurations,
-    urlsConfig = configurations.urls;
-var starString = "**********************************************************************",
-starStringWithNewLine = "**********************************************************************\r\n",
-tollString = "",
-d = new Date(),
-        dformat = [(d.getMonth() + 1),
-                d.getDate(),
-                d.getFullYear()].join('-'),
-logAppend = function (str) {
-    tollString += str;
-}
+    urlsConfig = configurations.urls,
+    starString = "**********************************************************************",
+    starStringWithNewLine = "**********************************************************************\r\n",
+    tollString = "",
+    d = new Date(),
+    dformat = [(d.getMonth() + 1),
+            d.getDate(),
+            d.getFullYear()].join('-'),
+    logAppend = function (str) {
+        tollString += str;
+    };
 
 // Reteving the productstage table data.
 var getOptions = {
@@ -22,22 +30,23 @@ var getOptions = {
     url: urlsConfig.productsGetall,
     headers: {
         'Content-Type': 'application/json'
-    },
+    }
 };
 var parsed = [];
+
 request(getOptions, function (error, response, body) {
     parsed = JSON.parse(body);
     logAppend(starStringWithNewLine);
-    logAppend("Total Products Extracted From Products Table:: [" + parsed.length + "]\r\n")
-    console.log(parsed.length);
-    recordCount = parsed.length;
+    logAppend("Total Products Extracted From Products Table:: [" + parsed.length + "]\r\n");
+
+    console.log(starStringWithNewLine);
+    console.log("Total Products Extracted From Products Table:: [" + parsed.length + "]\r\n");
 
     parsed.forEach(function (verifierProduct) {
-        i++;
         http.get(verifierProduct.redirectUrl, function (response) {
 
             logAppend(starStringWithNewLine);
-            if (response.statusCode != 400 && response.statusCode != 500) {
+            if (response.statusCode !== 400 && response.statusCode !== 500) {
                 verifierProduct.isPublished = body.indexOf(verifierProduct.shortDescription) > -1 || body.indexOf(verifierProduct.discount) > -1 || body.indexOf(verifierProduct.actualPrice) > -1 || body.indexOf(verifierProduct.sellingPrice) > -1;
             }
             else {
@@ -46,41 +55,40 @@ request(getOptions, function (error, response, body) {
             }
             updateProduct(verifierProduct);
         });
-            exportToLog(tollString);
+        exportToLog(tollString);
     });
 });
+
 function updateProduct(pushingItem) {
-    url = urlsConfig.productUpdate + pushingItem.id;
-    //console.log("update url" + url);
-    var updateOptions = {
-        method: 'PUT',
-        url: url,
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: {
-            "categoryId": pushingItem.categoryId,
-            "shortDescription": pushingItem.shortDescription,
-            "description": pushingItem.description,
-            "redirectUrl": pushingItem.redirectUrl,
-            "imageUrl": pushingItem.imageUrl,
-            "storeName": pushingItem.storeName,
-            "actualPrice": pushingItem.actualPrice,
-            "currentPrice": pushingItem.currentPrice,
-            "discountPercentage": pushingItem.discountPercentage,
-            "isShippingFree": pushingItem.isShippingFree,
-            "star": pushingItem.star,
-            "isPublished": pushingItem.isPublished,
-            "showDate": pushingItem.showDate,
-            "source": pushingItem.source,
-            //"createdDate": "1/1/2015",
-            //"lastUpdateDate": "1/1/2015"
-        }
-    };
+    var url = urlsConfig.productUpdate + pushingItem.id,
+        updateOptions = {
+            method: 'PUT',
+            url: url,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            json: {
+                "categoryId": pushingItem.categoryId,
+                "shortDescription": pushingItem.shortDescription,
+                "description": pushingItem.description,
+                "redirectUrl": pushingItem.redirectUrl,
+                "imageUrl": pushingItem.imageUrl,
+                "storeName": pushingItem.storeName,
+                "actualPrice": pushingItem.actualPrice,
+                "currentPrice": pushingItem.currentPrice,
+                "discountPercentage": pushingItem.discountPercentage,
+                "isShippingFree": pushingItem.isShippingFree,
+                "star": pushingItem.star,
+                "isPublished": pushingItem.isPublished,
+                "showDate": pushingItem.showDate,
+                "source": pushingItem.source
+            }
+        };
     request(updateOptions, function (error, response, body) {
         logAppend("Updated URL ID:: [" + pushingItem.id + "\r\n");
     });
 }
+
 function append(file, content, callback) {
     if (fs.appendFile) {
         fs.appendFile(file, content, callback);
@@ -96,9 +104,5 @@ function exportToLog(str) {
         if (err) {
             return console.log(err);
         }
-        console.log("The file was saved!");
     });
-
-    setTimeout(function () {
-    }, Math.floor((Math.random() * 500) + 1000));
 }
